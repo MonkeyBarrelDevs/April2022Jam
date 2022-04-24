4860f244f624f366b129989e5889684c7221dceb
Sebastien L Combes <fartingpickles@gmail.com>
2022-04-24 15:17:23 -0600

    Added scene music

--- April2022Jam/Assets/Scripts/AudioPlayer.cs
+++ April2022Jam/Assets/Scripts/AudioPlayer.cs
@@ -11,7 +11,7 @@ public class AudioPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        manager = FindObjectOfType<AudioManager>();
+        manager = GameObject.FindGameObjectWithTag("MainAudioManager").GetComponent<AudioManager>();
         if (play != "")
             manager.Play(play);
         if (stop != "")
