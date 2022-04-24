176c630834f4cd606ee4675c77020ab20730046d
Sebastien L Combes <fartingpickles@gmail.com>
2022-04-24 15:06:18 -0600

    Fixed isDead

--- April2022Jam/Assets/Scripts/PlayerAnimController.cs
+++ April2022Jam/Assets/Scripts/PlayerAnimController.cs
@@ -16,29 +16,26 @@ public class PlayerAnimController : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        if (!isDead)
-        {
-            #region Player
-            if (rb.velocity.x > xTolerance)
-                playerModel.localScale = new Vector3(1, 1, 1);
-            if (rb.velocity.x < -xTolerance)
-                playerModel.localScale = new Vector3(-1, 1, 1);
+        #region Player
+        if (rb.velocity.x > xTolerance)
+            playerModel.localScale = new Vector3(1, 1, 1);
+        if (rb.velocity.x < -xTolerance)
+            playerModel.localScale = new Vector3(-1, 1, 1);
 
-            playerAnim.SetFloat("Y", rb.velocity.y);
-            if (Mathf.Abs(rb.velocity.y) > yTolerance)
-                playerAnim.SetFloat("X", 0);
-            else
-                playerAnim.SetFloat("X", Mathf.Abs(rb.velocity.x));
-            #endregion
+        playerAnim.SetFloat("Y", rb.velocity.y);
+        if (Mathf.Abs(rb.velocity.y) > yTolerance)
+            playerAnim.SetFloat("X", 0);
+        else
+            playerAnim.SetFloat("X", Mathf.Abs(rb.velocity.x));
+        #endregion
 
-            #region Gun
-            float gunRotation = gunModel.rotation.eulerAngles.z;
-            if (gunRotation < 90 || gunRotation > 270)
-                gunModel.localScale = new Vector3(1, 1, 1);
-            if (gunRotation > 90 && gunRotation < 270)
-                gunModel.localScale = new Vector3(1, -1, 1);
-            #endregion
-        }
+        #region Gun
+        float gunRotation = gunModel.rotation.eulerAngles.z;
+        if (gunRotation < 90 || gunRotation > 270)
+            gunModel.localScale = new Vector3(1, 1, 1);
+        if (gunRotation > 90 && gunRotation < 270)
+            gunModel.localScale = new Vector3(1, -1, 1);
+        #endregion
     }
 
     public void Die()
