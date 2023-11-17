aa2e2f29d6da59582a6007095778691b7a10b9e4
TheIronFortress <70601770+TheIronFortress@users.noreply.github.com>
2023-11-16 17:01:31 -0700

    Update StargunController.cs
    Comments

--- April2022Jam/Assets/Scripts/StargunController.cs
+++ April2022Jam/Assets/Scripts/StargunController.cs
@@ -40,25 +40,34 @@ public class StargunController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Handles what happens when the player attempts to fire the star gun.
+    /// </summary>
     void Shoot()
     {
         if (ammo > 0)
         {
+            // Trigger the SFX and animations.
             string soundName = soundNames[UnityEngine.Random.Range(0, soundNames.Length)];
             GetComponent<AudioManager>().PlayOneShot(soundName);
             animController.Fire();
+
+            // Instantiate a star ammo gameobject that the player can pick up. 
             Instantiate(prefab, firePoint.position, firePoint.rotation);
+
+            // Calculate the force of the gunshot using the positions of the player and the mouse.
             Vector2 position2D = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
             Vector2 gunVector = (position2D - mousePosition).normalized;
-
             Vector2 gunForce = new Vector2(gunVector.x * xRecoilModifier, gunVector.y * yRecoilModifier);
+
+            // Maintain some of the player velocity if ared launched in the direction they're moving.
             if (rb.velocity.x * gunVector.x > 0)
                 gunForce.x += rb.velocity.x * xMomentumModifier;
             if (rb.velocity.y * gunVector.y > 0)
                 gunForce.y += rb.velocity.y * yMomentumModifier;
 
+            // Resolve the effects of shooting.
             rb.velocity = gunForce;
-            //rb.AddForce((gameObject.transform.position-mousePosition).normalized * recoilForce, ForceMode2D.Impulse);
             ammo--;
         }
     }
